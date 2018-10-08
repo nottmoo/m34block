2989f9aa673c0cb157eb17c3e1303a8e8d469d48
Yijun Xue <xuey@hotmail.com>
2018-10-08 15:15:47 +0100

    add travis yml

---
+++ settings.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * m34block block caps.
+ *
+ * @package    block_m34block
+ * @copyright  Daniel Neis <danielneis@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$settings->add(new admin_setting_heading('sampleheader',
+                                         get_string('headerconfig', 'block_m34block'),
+                                         get_string('descconfig', 'block_m34block')));
+
+$settings->add(new admin_setting_configcheckbox('m34block/foo',
+                                                get_string('labelfoo', 'block_m34block'),
+                                                get_string('descfoo', 'block_m34block'),
+                                                '0'));
